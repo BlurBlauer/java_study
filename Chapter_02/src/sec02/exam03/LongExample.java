9c61e93094a35a47e45a51097a2eccaebbbefac8
SeongYoon <blurblauer@gmail.com>
2022-02-18 00:44:00 +0900

    [upsrdate] 2/18/22, 00:43am

---
+++ Chapter_02/src/sec02/exam03/LongExample.java
@@ -0,0 +1,16 @@
+package sec02.exam03;
+
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+		//long var3 = 1000000000000;
+		long var4 = 1000000000000L; 
+		//정수 리터럴이 int 타입의 허용범위를 초과하는 경우, long 타입임을 컴파일러에게 알려줍니다.
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+	}
+
+}
