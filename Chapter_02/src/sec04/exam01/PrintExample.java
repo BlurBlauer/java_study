ef4b01d46618fe6b862112636c9ade0c751f73a1
SeongYoon <blurblauer@gmail.com>
2022-03-02 23:47:08 +0900

    [update] 3/2/22, 23:47pm

--- Chapter_02/src/sec04/exam01/PrintExample.java
+++ Chapter_02/src/sec04/exam01/PrintExample.java
@@ -13,10 +13,10 @@ public class PrintExample {
 		double area = 3.14159 * 10 * 10;
 		System.out.printf("반지름이 %d인 원의 없이: %10.2f\n", 10, area);
 		
-		
+		String name = "홍길동";
 		String job = "도적";
 		System.out.printf("%6d | %-10s | %10s\n", 1, name, job);
-		String name = "홍길동";
+		
 	}
 
 }
