7368d31349e114f98adf8da6f0c47b330a1a4c07
SeongYoon <blurblauer@gmail.com>
2022-03-29 23:53:20 +0900

    [update] 3/29/22, 23:53pm

---
+++ Chapter_06/src/sec03/exam01/CarExample.java
@@ -0,0 +1,11 @@
+package sec03.exam01;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("검정", 3000);
+		//Car myCar = new Car(); --> 생성자 클래스에서 파라미터를 초기화하였기 때문.
+
+	}
+
+}
