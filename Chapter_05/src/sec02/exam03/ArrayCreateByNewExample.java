8b3d854daf40ec37d647b0e6534e81825a1ec6d2
SeongYoon <blurblauer@gmail.com>
2022-04-05 23:36:10 +0900

    [update] 4/5/22, 23:46pm

--- Chapter_05/src/sec02/exam03/ArrayCreateByNewExample.java
+++ Chapter_05/src/sec02/exam03/ArrayCreateByNewExample.java
@@ -15,7 +15,6 @@ public class ArrayCreateByNewExample {
 			System.out.println("arr1[" + i +"]: " + arr1[i]);
 		}
 		
-		
 		double[] arr2 = new double[3];
 		for(int i=0; i<3; i++) {
 			System.out.println("arr2[" + i +"]: " + arr2[i]);
