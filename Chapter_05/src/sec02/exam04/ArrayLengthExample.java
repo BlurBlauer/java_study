e63c2208b87d9760763e5befb42cc3c4e9708ad3
SeongYoon <blurblauer@gmail.com>
2022-04-14 23:45:02 +0900

    [update] 4/14/22, 23:44pm

--- Chapter_05/src/sec02/exam04/ArrayLengthExample.java
+++ Chapter_05/src/sec02/exam04/ArrayLengthExample.java
@@ -9,6 +9,7 @@ public class ArrayLengthExample {
 		for(int i = 0; i<scores.length; i++) {
 			sum += scores[i];
 		}
+		
 		System.out.println("총합: " + sum);
 		double avg = (double) sum / scores.length;
 		System.out.println("평균: " + avg);
